c4220913fa58aa67aa834a6990764edc94addc86
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-05-11 13:31:23 -0700

    Update ball_bin example to use a blend file
    Improve shutdown and tmpdir cleanup logic.
    
    Also generally tidy up the README and etc.

--- examples/test/ball_bin_test.py
+++ examples/test/ball_bin_test.py
@@ -13,8 +13,14 @@ class BallBinTest(unittest.TestCase):
         # Find the server.
         demo_path = Path("examples/ball_bin").absolute().resolve()
         self.assertTrue(demo_path.exists(), demo_path)
+        settings = Path("examples/test/bpy_use_cycles.py").absolute().resolve()
+        self.assertTrue(settings.exists(), settings)
         # Run it.
-        result = subprocess.run([demo_path, "--still"], cwd=tmpdir)
+        result = subprocess.run([
+            demo_path,
+            "--still",
+            f"--bpy_settings_file={settings}"
+        ], cwd=tmpdir)
         result.check_returncode()
         self.assertTrue((tmpdir / "vtk_camera.png").exists())
         self.assertTrue((tmpdir / "blender_camera.png").exists())
