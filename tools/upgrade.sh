1c728b344266a4472b7be6650c5b2eb631d24d25
Jeremy Nimmer <jeremy.nimmer@tri.global>
2023-08-28 13:35:11 -0700

    [workspace] Add upgrade automation for all remaining dependencies (#60)

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -4,19 +4,15 @@
 # This script is intended for use by drake-blender developers.
 # Users of the project do not need to run it.
 
-# This script upgrades the pinned version of all dependencies, as captured
-# by the requirements.txt files that are commited into git as well as the
-# bazeliskrc file.
-
-# It does not automatically upgrade our version of bazelisk, which is for now
-# hard-coded inside our `./bazel` wrapper file.
+# This script upgrades the pinned version of all dependencies.
 
 set -eu -o pipefail
 
 me=$(python3 -c 'import os; print(os.path.realpath("'"$0"'"))')
 cd $(dirname "$me")/..
 
-python3 -B ./tools/requirements_upgrade_bazel.py
+python3 -B ./tools/upgrade_helper.py
+./bazel run //tools:buildifier tools/workspace_versions.bzl
 
 ./bazel run //:requirements.update -- --upgrade
 ./bazel run //:test_requirements.update -- --upgrade
