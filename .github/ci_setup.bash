d21878970b6cba13c503342bafad980114e73380
Jeremy Nimmer <jeremy.nimmer@tri.global>
2024-11-18 07:18:47 -0800

    [examples] Drop CI requirement for DISPLAY (#95)

--- .github/ci_setup.bash
+++ .github/ci_setup.bash
@@ -2,25 +2,5 @@
 
 set -euxo pipefail
 
-# Downgrade mesa per https://github.com/RobotLocomotion/drake/issues/18726.
-sudo apt-get --assume-yes --allow-downgrades install \
- xvfb \
- libegl1 \
- libegl-mesa0=22.0.1-1ubuntu2 \
- libgbm1=22.0.1-1ubuntu2 \
- libgl1-mesa-dri=22.0.1-1ubuntu2 \
- libglapi-mesa=22.0.1-1ubuntu2 \
- libglx-mesa0=22.0.1-1ubuntu2
-
-cat << EOF | sudo tee /lib/systemd/system/xvfb.service
-[Unit]
-After=network.target
-
-[Service]
-ExecStart=/usr/bin/Xvfb :99 -screen 0 1280x1024x24 -ac +extension GLX +extension RANDR +render -noreset
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo systemctl --now --quiet enable /lib/systemd/system/xvfb.service
+# Rendering in CI (for both Drake and Blender) requires a GL pipeline.
+sudo apt-get --assume-yes install libegl1
